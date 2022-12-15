6af8548a54712279ab8725ccdda01671ec2e020b
maria1120 <74187201+maria1120@users.noreply.github.com>
2022-12-15 14:13:15 +0000

    Add files via upload

--- MetroAllProjects-main/MetroAllProjects/MetroClient/src/test/java/com/emily/demo/MetroClientApplicationTests.java
+++ MetroAllProjects-main/MetroAllProjects/MetroClient/src/test/java/com/emily/demo/MetroClientApplicationTests.java
@@ -3,6 +3,7 @@ package com.emily.demo;
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 import com.emily.entity.Customer;
 import com.emily.persistance.TripDao;
@@ -34,6 +36,8 @@ class MetroClientApplicationTests {
 	private ClientServiceImpl service;
 	@Mock
 	private TripDao dao;
+	@Mock
+	RestTemplate restTemplate;
 	private AutoCloseable autoCloseable;
 
 	@BeforeEach
@@ -51,7 +55,8 @@ class MetroClientApplicationTests {
 	@Test
 	public void loginCheckTest() {
 		LocalDate dob = LocalDate.of(1997, 8, 17);
-		Customer customer = new Customer(101,"Bobby", "Moore", "bobby@gmail.com",dob,100);
+		Customer customer=new Customer(101,"Bobby", "Moore", "bobby@gmail.com",dob,100);
+		when(restTemplate.getForObject("http://localhost:8089/customers/"+101, Customer.class).thenReturn(customer);
 		
 		assertEquals(customer, service.loginCheck(101));
 	}
